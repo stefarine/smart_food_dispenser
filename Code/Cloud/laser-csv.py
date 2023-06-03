9848cc47ab4ac6b4f93ae6c66571870a41fd6258
GiacomoRattazzi <114418718+GiacomoRattazzi@users.noreply.github.com>
2023-06-03 22:29:29 +0200

    Update laser-csv.py

--- Code/Cloud/laser-csv.py
+++ Code/Cloud/laser-csv.py
@@ -21,11 +21,28 @@ def write_csv(request):
 
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(csv_file_name)
+    
+    # Read the existing content of the CSV file
+    try:
+        existing_content = blob.download_as_text()
+        first_time = False
+    except Exception as e:
+        print(f"Error downloading existing content: {e}")
+        existing_content = ""
+        first_time = True
 
-    csv_content = ""
-    for row in data:
-        csv_content += ",".join(map(str, row)) + "\n"
+    # Write column names if it's the first time creating the CSV file
+    if first_time:
+        column_names = "time,empty\n"
+    else:
+        column_names = ""
 
-    blob.upload_from_string(csv_content, content_type="text/csv")
-
-    return f"CSV file '{csv_file_name}' created in bucket '{bucket_name}'."
+    # Append new data with the current time
+    new_data = f"{current_time()},{data}\n"
+    updated_content = column_names + existing_content + new_data
+    
+    # Write the updated content to the CSV file
+    blob.upload_from_string(updated_content, content_type="text/csv")
+    print(f"Attempting to write to bucket: {bucket_name}")
+    print(f"Content to write: {updated_content}")
+    return f"Data appended to CSV file '{csv_file_name}' in bucket '{bucket_name}'."
